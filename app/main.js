9203631f6c04994ec4a3c72c745c23811c289f7a
Tharun Raj <tharun.2.linux@gmail.com>
2024-06-12 16:40:49 +0530

    add email validation

--- app/main.js
+++ app/main.js
@@ -1,4 +1,24 @@
+import validator from "validator";
+import isEmail from "validator/lib/isEmail";
 import "./typeScales.css";
 
 const d = new Date().getFullYear();
 document.getElementById("year").textContent = d;
+
+const form = document.getElementById("form");
+const formInput = document.getElementById("emailInput");
+const emailStatus = document.querySelector(".form__label");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!validator.isEmail(formInput.value)) {
+    emailStatus.textContent = "Please enter a valid email";
+    emailStatus.classList.add("status-error");
+    emailStatus.classList.remove("status-success");
+  } else {
+    emailStatus.textContent = "Success! Please check your inbox.";
+    emailStatus.classList.remove("status-error");
+    emailStatus.classList.add("status-success");
+  }
+});
